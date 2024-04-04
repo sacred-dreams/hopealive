ce0c6ddcd46432d160c7648820fa5971671589c9
sacred-dreams <164699165+sacred-dreams@users.noreply.github.com>
2024-04-04 01:00:25 -0400

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+
+// Function to load the videos page
+        function loadVideosPage() {
+        window.location.href = "../templates/videos.html"; // Redirect to videos page
+  }
+        
+    
+    function reloadIndex() {
+        window.location.href = "../index.html"; // Reload the current page (index.html)
+    }
+
+      
+    function donation() {
+        window.location.href = "../templates/donate.html"; // Reload the current page (index.html)
+    }  
+
+   function involved() {
+        window.location.href = "../templates/getinvolved.html"; // Reload the current page (index.html)
+    }  
+
+    function membership() {
+        window.location.href = "../templates/members.html"; // Reload the current page (index.html)
+    }  
+    
+     function about() {
+        window.location.href = "../templates/about.html"; // Reload the current page (index.html)
+    }  
+
+     function contact() {
+    window.location.href = "../templates/contact.html";
+}
+
+    showContent()
